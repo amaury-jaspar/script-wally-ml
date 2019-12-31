e36b8e760873d1980e4cf7a5df206f82f87c4c0b
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2019-12-31 16:14:47 +0100

    fixed input type error in main

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     crp.crop_all()
 
     proportion = 0.3 # Default value
-    proportion = int(input("Proportion of test images (float or int) : "))
+    proportion = float(input("Proportion of test images (float or int) : "))
 
     split.split(('true','false'), proportion)
 
